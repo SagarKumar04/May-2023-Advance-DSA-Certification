082f92a71da2f54f94471d2052cf3d97c3b97faf
Sagar <sagar@Sagars-MacBook-Air.local>
2023-06-25 11:49:06 +0530

    Fibonacci (using Tabulation and Memoization)

---
+++ DynamicProgramming/FibonacciUsingMemoization.java
@@ -0,0 +1,24 @@
+package DynamicProgramming;
+
+public class FibonacciUsingMemoization {
+    static int fibHelper(int n, int fibonacci[]) {
+        if(n <= 1) {
+            return n;
+        }
+
+        if(fibonacci[n] != 0) {
+            return fibonacci[n];
+        }
+
+        fibonacci[n] = fibHelper(n - 1, fibonacci) + fibHelper(n - 2, fibonacci);
+
+        return fibonacci[n];
+    }
+
+    static int fib(int n){
+        //Write your code here
+        int fibonacci[] = new int[n + 1];
+
+        return fibHelper(n, fibonacci);
+    }
+}
---
+++ DynamicProgramming/FibonacciUsingTabulation.java
@@ -0,0 +1,18 @@
+package DynamicProgramming;
+
+public class FibonacciUsingTabulation {
+    static int fib(int n) {
+        //Write your code here
+        if(n == 0) {
+            return 0;
+        }
+        int fibonacci[] = new int[n + 1];
+        fibonacci[1] = 1;
+
+        for(int i = 2; i <= n; i++) {
+            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+        }
+
+        return fibonacci[n];
+    }
+}

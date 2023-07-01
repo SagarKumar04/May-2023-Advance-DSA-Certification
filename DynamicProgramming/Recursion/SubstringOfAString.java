cc8e5d8df8d0dfa3581675d6a3be67f2616ada71
Sagar <sagar@Sagars-MacBook-Air.local>
2023-07-01 14:51:44 +0530

    Permutation of a String

---
+++ DynamicProgramming/Recursion/SubstringOfAString.java
@@ -0,0 +1,45 @@
+package DynamicProgramming.Recursion;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class SubstringOfAString {
+    static List<String> resultList = new ArrayList<String>();
+
+    private static void swap(char strArr[], int index1, int index2) {
+        char temp = strArr[index1];
+        strArr[index1] = strArr[index2];
+        strArr[index2] = temp;
+    }
+
+    private static void permute(char strArr[], int left, int right) {
+        if(left == right) {
+            String str = new String(strArr);
+            resultList.add(str);
+            return;
+        }
+
+        for(int i = left; i <= right; i++) {
+            swap(strArr, left, i);
+            permute(strArr, left + 1, right);
+            swap(strArr, left, i);
+        }
+    }
+
+    public static void main (String[] args) {
+        // Your code here
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+
+        char strArr[] = str.toCharArray();
+
+        permute(strArr, 0, strArr.length - 1);
+
+        Collections.sort(resultList);
+        for(String result : resultList) {
+            System.out.print(result + " ");
+        }
+    }
+}

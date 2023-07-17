e963445dfd53864e33b50897fcb10d1fade14b35
Sagar <sagar@Sagars-MacBook-Air.local>
2023-07-17 17:21:26 +0530

    Cut the Rod

---
+++ DynamicProgramming/InClass/UnboundedKnapsack/CutTheRodDP.java
@@ -0,0 +1,30 @@
+package DynamicProgramming.InClass.UnboundedKnapsack;
+
+import java.util.Arrays;
+
+public class CutTheRodDP {
+    private static int getMaxProfitHelper(int a[], int n, int profit[]) {
+        if(n == 0) {
+            return 0;
+        }
+
+        if(profit[n] != -1) {
+            return profit[n];
+        }
+
+        int maxProfit = -1;
+        for(int i = 1; i <= n; i++) {
+            maxProfit = Math.max(maxProfit, a[i - 1] + getMaxProfitHelper(a, n - i, profit));
+        }
+
+        profit[n] = maxProfit;
+
+        return maxProfit;
+    }
+
+    public static int getMaxProfit(int[] a, int n) {
+        int profit[] = new int[n + 1];
+        Arrays.fill(profit, -1);
+        return getMaxProfitHelper(a, n, profit);
+    }
+}
---
+++ DynamicProgramming/InClass/UnboundedKnapsack/CutTheRodRecursion.java
@@ -0,0 +1,20 @@
+package DynamicProgramming.InClass.UnboundedKnapsack;
+
+public class CutTheRodRecursion {
+    private static int getMaxProfitHelper(int a[], int n) {
+        if(n == 0) {
+            return 0;
+        }
+
+        int maxProfit = -1;
+        for(int i = 1; i <= n; i++) {
+            maxProfit = Math.max(maxProfit, a[i - 1] + getMaxProfitHelper(a, n - i));
+        }
+
+        return maxProfit;
+    }
+
+    public static int getMaxProfit(int[] a, int n) {
+        return getMaxProfitHelper(a, n);
+    }
+}
